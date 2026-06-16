5205871f8e2299d0ef1f6d8ac85c48d429b7ed39
Anthony Diaz-Stack <adiazstack@gmail.com>
2026-06-16 16:07:37 -0400

    Camera Displacement
    Added camera displacement to follow the mouse better.

---
+++ Felathars/Assets/Scripts/cameraController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    [Range(0.1f, 0.2f)][SerializeField] float displacementMult;
+    GameObject player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offsetCamera();
+        
+        
+    }
+
+    void offsetCamera()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        if (groundPlane.Raycast(camRay, out float hitDist))
+        {
+            Vector3 mousePos = camRay.GetPoint(hitDist);
+            Vector3 playerPos = player.transform.position;
+            Vector3 camDisplacement = (mousePos - player.transform.position) * displacementMult;
+            Vector3 newCamPos = player.transform.position + camDisplacement;
+            newCamPos.y = 10;
+            transform.position = newCamPos;
+        }
+    }
+}
