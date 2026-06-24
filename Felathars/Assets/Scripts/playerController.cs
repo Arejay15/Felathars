cd80b2e5d4c5dc63a466863da36a9ad091df30e8
Marley P <ooebig@gmail.com>
2026-06-24 13:23:38 -0400

    Weapon Switching and Bullets
    Mostly, done, just making sure other's have access for now. Had some weird computer issues and github issues, but good now.

--- Felathars/Assets/Scripts/playerController.cs
+++ Felathars/Assets/Scripts/playerController.cs
@@ -10,33 +10,53 @@ public class playerController : MonoBehaviour, IDamage
 
     [SerializeField] CharacterController controller;
     [SerializeField] GameObject playerModel;
-    [SerializeField] public float HP;
-    [SerializeField] public int speed;
-    [SerializeField] public int sprintMod;
-    [SerializeField] gamemanager.ColorType defensiveColor;
-    [SerializeField] weapons.weaponTypes type;
-    [SerializeField] public float shootRate;
-    [SerializeField] float damageAmount;
-    [SerializeField] int shootDist;
-    [SerializeField] GameObject bullet;
+    [SerializeField, Range(25f, 250f)] public float HP;
+    [SerializeField, Range(5, 50)] public int speed;
+    [SerializeField, Range(1.1f, 3f)] public float sprintMod;
+    
     [SerializeField] Transform shootPos;
     [SerializeField] Transform gunPivot;
+    [SerializeField] Renderer gunModel;
+    [SerializeField, Range(0.1f, 10f)] float switchDelay;
+    [Header("Enabled Guns")]
+    [SerializeField] weapons whiteGun;
+    [SerializeField] weapons redGun;
+    [SerializeField] weapons greenGun;
+    [SerializeField] weapons blueGun;
+    [SerializeField] weapons yellowGun;
+
+    public float fireRateBuff = 1;
+    gamemanager.ColorType defensiveColor;
+    weapons[] gunList;
+    weapons activeGun;
+    int activeGunNum;
 
     public int damageReduction;
+    float shootRate;
     float shootTimer;
     public float originalHP;
     public float tempHP;
 
     Vector3 moveDir;
 
-    public float lookSens = 30;
+    int lookSens = 30;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         originalHP = HP;
         tempHP = 0;
-        type = weapons.weaponTypes.game;
+        gunList = new weapons[]{ whiteGun, redGun, greenGun, blueGun, yellowGun };
+        for (int i = 0; activeGun == null; i++)
+        {
+            activeGun = gunList[i];
+            activeGunNum = i;
+        }
+        gunModel.material.color = activeGun.materialColor;
+
+        shootRate = activeGun.fireRate;
+        defensiveColor = activeGun.colorType;
+
         updatePlayerUI();
     }
 
@@ -47,7 +67,42 @@ public class playerController : MonoBehaviour, IDamage
 
         movement();
 
+        selectGun();
+
         sprint(); //available as an upgrade?
+    }
+
+    void selectGun()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            
+            activeGunNum++;
+            activeGun = null;
+            for (int i = activeGunNum; activeGun == null; i++)
+            {
+                if (i == 5) { i = 0; }
+                activeGun = gunList[i];
+                activeGunNum = i;
+                
+            }
+            gunModel.material.color = activeGun.materialColor;
+            defensiveColor = activeGun.colorType;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            activeGunNum--;
+            activeGun = null;
+            for (int i = activeGunNum; activeGun == null; i--)
+            {
+                if (i == -1) { i = 4; }
+                activeGun = gunList[i];
+                activeGunNum = i;
+
+            }
+            gunModel.material.color = activeGun.materialColor;
+            defensiveColor = activeGun.colorType;
+        }
     }
 
     void movement()
@@ -57,6 +112,8 @@ public class playerController : MonoBehaviour, IDamage
         moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
         controller.Move(moveDir * speed * Time.deltaTime); 
         Debug.DrawRay(playerModel.transform.position, playerModel.transform.forward * 10, Color.red);
+
+        
 
         if (Input.GetButton("Fire1") && shootTimer > shootRate)
         {
@@ -70,11 +127,11 @@ public class playerController : MonoBehaviour, IDamage
     {
         if (Input.GetButtonDown("Sprint"))
         {
-            speed *= sprintMod;
+            speed = (int)(speed * sprintMod);
         }
         else if (Input.GetButtonUp("Sprint"))
         {
-            speed /= sprintMod;
+            speed = (int)(speed / sprintMod);
         }
     }
     
@@ -131,11 +188,41 @@ public class playerController : MonoBehaviour, IDamage
         gamemanager.instance.playerTempHPBar.fillAmount = (float)tempHP / originalHP;
     }
 
-    void shoot()
+    public void shoot()
     {
         shootTimer = 0;
-        Instantiate(bullet, shootPos.position, gunPivot.rotation);
-        
+        switch (activeGun.mode) {
+            case weapons.Mode.Burst:
+                burstshot();
+                break;
+            case weapons.Mode.Spread:
+
+                float gap = activeGun.spreadAngle / (activeGun.shotNum - 1);
+                float startAngle = -activeGun.spreadAngle / 2f;
+
+                for (int i = 0; i < activeGun.shotNum; i++)
+                {
+                    float angle = startAngle + gap * i;
+                    Quaternion rotation = gunPivot.rotation * Quaternion.Euler(0f, angle, 0f);
+                    Instantiate(activeGun.bullet, shootPos.position, rotation);
+                }
+
+                break;
+            default:
+                
+                Instantiate(activeGun.bullet, shootPos.position, gunPivot.rotation);
+                break;
+        }
+    }
+
+    IEnumerator burstshot()
+    {
+        Instantiate(activeGun.bullet, shootPos.position, gunPivot.rotation);
+        for (int i = activeGun.shotNum - 1; i > 0; i--)
+        {
+            yield return new WaitForSeconds(activeGun.burstSpeed);
+            Instantiate(activeGun.bullet, shootPos.position, gunPivot.rotation);
+        }
     }
 
     IEnumerator flashDamage()
