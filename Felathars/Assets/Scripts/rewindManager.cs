dd8fd17839ed230a27ff52f3c8ca0cbdacc78e8e
Anthony Diaz-Stack <adiazstack@gmail.com>
2026-06-25 02:54:15 -0400

    Added base rewind
    Base rewind time code added, still needs some adjustments, will get finished shortly

--- Felathars/Assets/Scripts/rewindManager.cs
+++ Felathars/Assets/Scripts/rewindManager.cs
@@ -1,16 +1,39 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class rewindManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    private static List<rewindObject> rewindObjects = new List<rewindObject>();
+    private static bool isRewinding = false;
+    public static void Register(rewindObject obj) {
+        if (!rewindObjects.contains(obj)) rewindObjects.Add(obj);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public static void Unregister(rewindObject obj) {
+        rewindObjects.Remove(obj);
+    }
+
+    public void StartRewindAll() {
+        isRewinding = true;
+        foreach (var obj in rewindObjects) {
+            obj.StartRewind();
+        }
+    }
+
+    public void StopRewindAll() {
+        isRewinding = false;
+        foreach (var obj in rewindObjects) {
+            obj.StopRewind();
+        }
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Beanback")) {
+            StartRewindAll();
+        }
+        if (Input.GetButtonUp("Beanback")){
+            StopRewindAll();
+        }
+    
     }
 }
--- Felathars/Assets/Scripts/rewindObject.cs
+++ Felathars/Assets/Scripts/rewindObject.cs
@@ -1,16 +1,67 @@
+using NUnit.Framework;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class rewindObject : MonoBehaviour
 {
+    private bool isRewinding = false;
+    private Rigidbody rb;
+    private List<TransformData> history = new List<TransformData>();
+    [SerializeField] private float recordTime = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb.GetComponent<Rigidbody>();
+        rewindManager.Register(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDestroy()
     {
-        
+        rewindManager.Unregister(this);    
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRewinding) Rewind();
+        else Record();
+    }
+    
+    void Record() {
+        if (history.Count > Mathf.Round(recordTime / Time.fixedDeltaTime))
+        {
+            history.RemoveAt(history.Count - 1);
+        }
+        history.Insert(0, new TransformData(transform.position, transform.rotation));
+    }
+
+    void Rewind() {
+        if (history.Count > 0) {
+            TransformData data = history[0];
+            transform.position = data.position;
+            transform.rotation = data.rotation;
+            history.RemoveAt(0);
+        }
+    }
+
+    public void StartRewind() {
+        isRewinding = true;
+        if (rb != null) rb.isKinematic = true;
+    }
+    public void StopRewind(){
+        isRewinding = false;
+        if (rb != null) rb.isKinematic = false;
+    }
+
+    private struct TransformData {
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public TransformData(Vector3 pos, Quaternion rot) {
+            position = pos;
+            rotation = rot;
+        }
     }
 }
